90e596973780e72d8f8e0b9e9cf89cac8c672bd0
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-07-03 12:19:02 +0600

    lottie animation in banner

--- src/components/pages/Home/Banner/Banner.jsx
+++ src/components/pages/Home/Banner/Banner.jsx
@@ -1,20 +1,38 @@
 
 import banner from '../../../../assets/HomeBanner.jpg'
+import Lottie from 'react-lottie';
+import animationData from '../../../../assets/118785-responsive-website-design.json';
+
 
 const Banner = () => {
+    const defaultOptions = {
+        loop: true, // Set it to false if you don't want the animation to loop
+        autoplay: true, // Set it to false if you don't want the animation to play automatically
+        animationData: animationData, // Provide the imported animation JSON file
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    };
     return (
-        <div className="hero h-[250ox] md:h-[650px] " style={{ backgroundImage: `url(${banner})` }}>
+        <div className="hero h-[250ox] md:h-[450px] ">
             <div className="hero-overlay bg-opacity-60"></div>
             <div className='container mx-auto'>
                 <div className="hero-content text-neutral-content">
-                    <div className="w-full">
+                    <div className="w-full md:w-3/4">
                         <h3 className='font-semibold text-xl text-cyan-400 mb-5'> Hi, I&apos;m </h3>
                         <h1 className="mb-5 text-7xl font-bold"> <span className='text-cyan-400'>Shahadat</span> Rafi</h1>
                         <p className="mb-5 max-w-xl text-xl">Passionate developer empowering businesses with tailored web solutions for seamless user experiences. Let&apos;s transform your vision into success.</p>
                         <button className="btn bg-cyan-400 text-black tracking-wider hover:bg-cyan-600 ">Hire Me</button>
                     </div>
+                    <div className="w-full md:w-1/2">
+                        <div>
+                            <Lottie options={defaultOptions} />
+                        </div>
+                    </div>
+
                 </div>
             </div>
+
         </div>
     );
 };
